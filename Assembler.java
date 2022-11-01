762b6bbaa93ab68d4698e36bebee9735229023d4
dandrecampbell <dandrecampbell5@gmail.com>
2022-10-31 21:05:34 -0400

    Finished. with tests

--- Assembler.java
+++ Assembler.java
@@ -1,32 +1,98 @@
 public class Assembler {
-    
-    public static String[] assemble(String[] args) {
-        String[] output = {""};
+
+    //public static final String[] keywords = {"move", "halt", "interrupt"};
+
+    public static String[] assemble(String[] instructions) {
+        String[] output = new String[instructions.length];
+
+        for(int i = 0; i < instructions.length; i++) {
+            String[] command = instructions[i].split(" ");
+
+            if(command[0].equalsIgnoreCase("move")) {
+                output[i] = "0001";
+                output[i] += get_register(command[1]);
+                output[i] += get_value(command[2]); // last 8 bits / value to be moved to an address
+            }
+            else if(command[0].equalsIgnoreCase("halt")) {
+                output[i] = "0000000000000000";
+            }
+            else if(command[0].equalsIgnoreCase("interrupt")) {
+                if(command[1].equals("0")) {
+                    output[i] = "0010000000000000";
+                }
+                else if(command[1].equals("1")) {
+                    output[i] = "0010000000000001";
+                }
+            }
+        }
         
         return output;
     }
 
-    public static void lexing() {
+    /*
+        Helper method for the move command.
+        Returns the register address for as a 4 bit string.
+    */
+    public static String get_register(String arg) {
+        String output = "";
+        String temp_register = arg.replaceAll("R", "");
+        int address = Integer.parseInt(temp_register);
 
-    }
+        // Must be within range of 16 possible registers
+        if(address > 15) {
+            output = "Address is out of range.";
+        }
 
-    public static void parsing() {
+        // Converts address to a binary array
+        int[] binary = new int[4];
+        int i = 0;
+        while(address > 0) {
+            binary[i] = address % 2;
+            address /= 2;
+            i++;
+        }
 
-    }
-
-    public static Bit[] bit_pattern(Longword register_name) {
-        Bit[] pattern = new Bit[4];
-
-        for(int i = 4; i >= 0; i--) {
-            if(register_name.getBit(i).getValue() == true) {
-                pattern[i].set(true);
+        // Converts binary array to a string
+        for(i = 3; i >= 0; i--) {
+            if(binary[i] == 1) {
+                output += "1";
             }
-            else if(register_name.getBit(i).getValue() == false) {
-                pattern[i].set(false);
+            else if(binary[i] == 0) {
+                output += "0";
             }
         }
 
-        
-        return pattern;
+        return output;
+    }
+
+    /*
+        Helper method for move command.
+        Returns the value that'll be stored in a register
+        as an 8 bit string.
+    */
+    public static String get_value(String arg) {
+        String output = "";
+        int temp_value = Integer.parseInt(arg);
+        int[] binary = new int[8];
+
+        // Converts decimal value to a binary array
+        int i = 0;
+        while(temp_value > 0) {
+            binary[i] = temp_value % 2;
+            temp_value /= 2;
+            i++;
+        }
+
+        // Converts binary array to a string
+        for(i = 7; i >= 0; i--) {
+            if(binary[i] == 1) {
+                output += "1";
+            }
+            else if(binary[i] == 0) {
+                output += "0";
+            }
+        }
+
+        return output;
     }
 }
--- AssemblerTest.java
+++ AssemblerTest.java
@@ -1,51 +1,39 @@
 public class AssemblerTest {
     
-    public static void test_assemble() throws Exception {
+    public static void test_assemble1() {
+        try {
+            String[] commands = {"move R1 10", "halt", "interrupt 1", "interrupt 0"};
 
+            for(int i = 0; i < commands.length; i ++) {
+                System.out.printf("%s: %s \n", commands[i], Assembler.assemble(commands)[i]);
+            }
+            System.out.println();
+        }
+        catch(Exception e) {
+            System.out.println("Assembler Failed.");
+            e.printStackTrace();
+        }
     }
 
-    public static void test_lexing() throws Exception {
+    public static void test_assemble2() {
+        try {
+            String[] commands = {"interrupt 0", "move R15 100", "halt", "interrupt 1"};
 
+            for(int i = 0; i < commands.length; i ++) {
+                System.out.printf("%s: %s \n", commands[i], Assembler.assemble(commands)[i]);
+            }
+            System.out.println();
+        }
+        catch(Exception e) {
+            System.out.println("Assembler Failed.");
+            e.printStackTrace();
+        }
     }
 
-    public static void test_parsing() throws Exception {
+    public static void runTest() {
+        System.out.println("Assembler Test ");
 
-    }
-
-    public static void test_pattern() throws Exception {
-        Longword lw = new Longword();
-        Bit[] test_pattern = new Bit[4]; 
-        
-        lw = new Longword(13); // ttft
-        test_pattern[0] = new Bit(true);
-        test_pattern[1] = new Bit(false);
-        test_pattern[2] = new Bit(true);
-        test_pattern[3] = new Bit(true);
-        if(Assembler.bit_pattern(lw).equals(test_pattern)) {
-            System.out.println("test_pattern() 1 - Success");
-        }
-        else {
-            throw new Exception("test_pattern 1 - Failed");
-        }
-
-        lw = new Longword(7); // fttt
-        test_pattern[0] = new Bit(true);
-        test_pattern[1] = new Bit(true);
-        test_pattern[2] = new Bit(true);
-        test_pattern[3] = new Bit(false);
-        if(Assembler.bit_pattern(lw).equals(test_pattern)) {
-            System.out.println("test_pattern() 2 - Success");
-        }
-        else {
-            throw new Exception("test_pattern 2 - Failed");
-        }
-
-    }
-
-    public static void runTest() throws Exception {
-        test_assemble();
-        test_lexing();
-        test_parsing();
-        test_pattern();
+        test_assemble1();
+        test_assemble2();
     }
 }
