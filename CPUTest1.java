a5f455febc8ce79e194118989443120de7a1b860
dandrecampbell <dandrecampbell5@gmail.com>
2022-11-06 02:41:16 -0500

    tests and comments

--- CPUTest1.java
+++ CPUTest1.java
@@ -16,8 +16,6 @@ public class CPUTest1 {
             System.out.println("Run Error.");
             e.printStackTrace();
         }
-
-        System.out.println();
     }
 
     public static void test_move() {
@@ -26,53 +24,58 @@ public class CPUTest1 {
             test_computer.move(new Longword(0), new Longword(333));
             test_computer.move(new Longword(1), new Longword(1000));
             test_computer.move(new Longword(2), new Longword(245));
+            System.out.println("move() - success");
+
             test_computer.interrupt_0();
+            System.out.println("interrupt_0() - success\n");
         }
         catch(Exception e) {
-            System.out.println("Move/Interrupt-0 Error.");
+            System.out.println("move()/interrupt_0()-0 error.");
             e.printStackTrace();
         }
-
-        System.out.println();
     }
 
     public static void test_halt() {
         try {
             Computer test_computer = new Computer();
             test_computer.halt();
+            System.out.println("halt() - success");
         }
         catch(Exception e) {
-            System.out.println("Halt Error.");
+            System.out.println("halt() Error.");
             e.printStackTrace();
         }
-
-        System.out.println();
     }
 
     public static void test_interrupt1() {
         try {
             Computer test_computer = new Computer();
             test_computer.interrupt_1();
+            System.out.println("interrupt_1() - success");
         }
         catch(Exception e) {
-            System.out.println("Interrupt-1 Error.");
+            System.out.println("interrupt_1()-1 error.");
             e.printStackTrace();
         }
         System.out.println();
     }
 
-    public static void test_alu1() {
+    public static void test_alu1() throws Exception {
         Bit[] test = new Bit[]{new Bit(), new Bit(true), new Bit(true), new Bit(true)}; //add
+        int result = ALU.doOp(test, new Longword(10), new Longword(2)).getSigned();
 
-        System.out.printf("""
-                Running ALU...
-                10 + 2: %d
-                """, ALU.doOp(test, new Longword(10), new Longword(2)).getSigned());
-
+        if(result != 12) {
+            throw new Exception("alu - failed");
+        }
+        else {
+            System.out.println("alu - success");
+            System.out.printf("Running ALU...\n10 + 2: %d\n", result);
+        }
+        
         System.out.println();
     }
 
-    public static void runTest() {
+    public static void runTest() throws Exception {
         System.out.println("CPU Test 1:");
 
         test_preload();
